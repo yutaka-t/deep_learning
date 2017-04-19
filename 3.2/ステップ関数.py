16e30364cc5bf2adce8365a6f933cbd92813fb52
yutaka <yutaka.tanoue@gmail.com>
2017-04-19 22:26:09 +0900

    no message

---
+++ 3.2/ステップ関数.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+import numpy as np
+import matplotlib.pylab as plt
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+plt.plot(x, y)
+
+# y軸の範囲指定
+plt.ylim(-0.1, 1.1)
+plt.show()
