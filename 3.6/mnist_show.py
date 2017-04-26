2d424a7ef7e11813d73cd6334b8f29abb7b303ef
yutaka <yutaka.tanoue@gmail.com>
2017-04-27 02:01:00 +0900

    3.6

---
+++ 3.6/mnist_show.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import sys
+import os
+
+sys.path.append(os.pardir)
+from dataset.mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+# それぞれのデータの形状を出力
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
